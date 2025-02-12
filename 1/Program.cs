6f1ea1d206fe5de92d6e44d6e018f9c00920209f
Lysande2902 <yenglee2006@gmail.com>
2025-02-12 10:28:56 -0600

    Ejercicio1-5 2P

---
+++ 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string nombres = "Joel";
+        string apellidos = "Pool";
+
+        Console.WriteLine("NombreApellido: " + nombres + " " + apellidos);
+        Console.WriteLine("ApellidoNombre: " + apellidos + ", " + nombres);
+    }
+}
